d24358c6824abb3c2e26d290f0957b1df5e0efaf
Tony Roscioli <adrprogramming@gmail.com>
2021-04-21 15:13:55 -0600

    fixed page

---
+++ bored copy.js
@@ -0,0 +1,5 @@
+var fart = new Audio();
+fart.src = "Fart_BW.16093.wav";
+function PlaySound() {
+    fart.play();
+}
---
+++ script copy.js
@@ -0,0 +1,24 @@
+face.addEventListener('mouseover',function(){
+    face.style.width = '200px';
+    face.style.height = '200px';});
+
+pup.addEventListener('mouseover',function(){
+    pup.style.width = '200px';
+    pup.style.height = '200px';});
+
+face.addEventListener('mouseout',function(){
+    face.style.width = '50px';
+    face.style.height = '50px';});
+    
+pup.addEventListener('mouseout',function(){
+    pup.style.width = '50px';
+    pup.style.height = '50px';});
+
+document.getElementById("html1").addEventListener("click", function() {
+    alert(`Hypertext Markup Language, or "HTML", is a coding language used for the structure of webpages. It is one of the main building blocks for the World Wide Web, along with CSS and JavaScript.`);});
+
+document.getElementById("css1").addEventListener("click", function() {
+    alert(`Cascading Style Sheets, or "CSS" is a styling language used for the presentation of webpages. CSS, alongside HTML and JavaScript, is a fundamental technology for the World Wide Web.`);});
+
+document.getElementById("js1").addEventListener("click", function() {
+    alert(`JavaScript is the language of the web! It is a programming language used to make webpages more interactive and dynamic. JavaScript, combined with HTML and CSS, makes the World Wide Web what it is today.`);});
